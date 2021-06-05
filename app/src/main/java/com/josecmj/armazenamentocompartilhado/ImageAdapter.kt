61196cab970f8ba22cc8f1e2598c7a18f8dcffad
Anderson Mendes de Almeida <ama@cesar.school>
2021-06-05 10:27:06 -0300

    [TASK]Adjustments to delete file from gallery

--- app/src/main/java/com/josecmj/armazenamentocompartilhado/ImageAdapter.kt
+++ app/src/main/java/com/josecmj/armazenamentocompartilhado/ImageAdapter.kt
@@ -1,17 +1,19 @@
 package com.josecmj.armazenamentocompartilhado
 
-import android.util.Log
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import java.io.File
+import java.net.URI
+import kotlin.reflect.KFunction0
 
-class ImageAdapter : ListAdapter<MyImage, ImageAdapter.FileViewHolder>(FileDiffCallback) {
+class ImageAdapter(val callback: (Uri) -> Unit) : ListAdapter<MyImage, ImageAdapter.FileViewHolder>(FileDiffCallback) {
 
     class FileViewHolder(itemView: View) :
         RecyclerView.ViewHolder(itemView)  {
@@ -23,7 +25,6 @@ class ImageAdapter : ListAdapter<MyImage, ImageAdapter.FileViewHolder>(FileDiffC
             currentFile = file
             fileTextView.text = file.displayName
             fileImageView.setImageURI(file.contentUri)
-
         }
 
         init {
@@ -34,12 +35,16 @@ class ImageAdapter : ListAdapter<MyImage, ImageAdapter.FileViewHolder>(FileDiffC
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FileViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.image_item, parent, false)
+
         return FileViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: FileViewHolder, position: Int) {
         val file = getItem(position)
         holder.bind(file)
+        holder.itemView.setOnClickListener {
+            callback(file.contentUri)
+        }
     }
 }
 object FileDiffCallback : DiffUtil.ItemCallback<MyImage>() {
